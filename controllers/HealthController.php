81534a15cf5af0979f12adbb50c2343aafca8212
Wavesonics <adamwbrown@gmail.com>
2016-07-18 21:40:51 -0700

    Health monitor now records the app version

--- controllers/HealthController.php
+++ controllers/HealthController.php
@@ -19,9 +19,11 @@ class HealthController extends Controller
     
     public function post( $request )
     {
-        if( count($request->args) == 1 && is_numeric($request->args[0]) )
+        if( count($request->args) == 1 && is_numeric($request->args[0]) && is_numeric($request->params['version']) )
         {
             $photoFrameId = $request->args[0];
+			
+			$photoFrameVersion = $request->params['version'];
 
             $errors = "no";
             if( !empty($request->post) )
@@ -30,6 +32,7 @@ class HealthController extends Controller
             }
             
             $item['photo_frame'] = $photoFrameId;
+			$item['version'] = $photoFrameVersion;
             $item['errors'] = $errors;
             
             $db = getDb();
