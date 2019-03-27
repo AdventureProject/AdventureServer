f007c42eecea5be66d234c608fe1be5b4d8cf8b7
Wavesonics <adamwbrown@gmail.com>
2019-03-26 23:40:10 -0700

    Added log list controller

--- controllers/base/LogController.php
+++ controllers/base/LogController.php
@@ -6,32 +6,32 @@ require_once('utils/b2_util.php');
 
 class LogController extends BaseController
 {
-    public function __construct( $config )
-    {
-        parent::__construct( false, $config );
-    }
-	
+	public function __construct( $config )
+	{
+		parent::__construct( false, $config );
+	}
+
 	public function urlStub()
 	{
 		return 'log';
 	}
-	
-    public function getTitle()
-    {
-    	return 'Log';
-    }
-	
+
+	public function getTitle()
+	{
+		return 'Log';
+	}
+
 	public function getRichTitle()
 	{
 		return 'Adventure.Rocks - Log';
 	}
-	
-    public function getBody( $request, $todaysPhoto, $xtpl )
-    {
+
+	public function getBody( $request, $todaysPhoto, $xtpl )
+	{
 		//$this->addCssFile( '/css/about.css', $xtpl );
 
-        $db = getDb();
-        $blogPost = $db->blogs[1];
+		$db = getDb();
+		$blogPost = $db->blogs[1];
 
 		$parsedown = new Parsedown();
 
@@ -39,16 +39,16 @@ class LogController extends BaseController
 		$bodyText = $parsedown->text( $markdownText );
 
 		$coverPhotoId = $blogPost['cover_photo_id'];
-		
-		$xtpl->assign_file('BODY_FILE', 'templates/log.html');
 
-        $xtpl->assign('BLOG_TITLE', $blogPost['title']);
-		$xtpl->assign('BLOG_CONTENT', $bodyText);
+		$xtpl->assign_file( 'BODY_FILE', 'templates/log.html' );
 
-        $xtpl->assign('PHOTO_THUMBNAIL', b2GetPublicBlurUrl( $coverPhotoId ));
+		$xtpl->assign( 'BLOG_TITLE', $blogPost['title'] );
+		$xtpl->assign( 'BLOG_CONTENT', $bodyText );
 
-        $xtpl->parse('main.body');
-    }
+		$xtpl->assign( 'PHOTO_THUMBNAIL', b2GetPublicBlurUrl( $coverPhotoId ) );
+
+		$xtpl->parse( 'main.body' );
+	}
 }
 
 ?>
---
+++ controllers/base/LogsController.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once('utils/BaseController.php');
+require_once('libs/Parsedown.php');
+require_once('utils/b2_util.php');
+
+class LogsController extends BaseController
+{
+	public function __construct( $config )
+	{
+		parent::__construct( false, $config );
+	}
+
+	public function urlStub()
+	{
+		return 'logs';
+	}
+
+	public function getTitle()
+	{
+		return 'Logs';
+	}
+
+	public function getRichTitle()
+	{
+		return 'Adventure.Rocks - Logs';
+	}
+
+	public function getBody( $request, $todaysPhoto, $xtpl )
+	{
+		$db = getDb();
+
+		$parsedown = new Parsedown();
+
+		foreach( $db->blogs() as $entry )
+		{
+			$content = $entry['content'];
+			$contentSummary = substr( $content, 0, strpos( $content, "\n" ) );
+
+			$bodyText = $parsedown->text( $contentSummary );
+
+			$coverPhotoId = $entry['cover_photo_id'];
+
+			$xtpl->assign_file( 'BODY_FILE', 'templates/logs.html' );
+
+			$xtpl->assign( 'ENTRY_ID', $entry['id'] );
+			$xtpl->assign( 'BLOG_TITLE', $entry['title'] );
+			$xtpl->assign( 'BLOG_CONTENT', $bodyText );
+
+			$xtpl->assign( 'PHOTO_THUMBNAIL', b2GetPublicBlurUrl( $coverPhotoId ) );
+			$xtpl->parse( 'main.body.log_entry' );
+		}
+
+		$xtpl->parse( 'main.body' );
+	}
+}
+
+?>
