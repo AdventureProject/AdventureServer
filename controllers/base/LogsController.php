90b9c763e6405f5bf3c49df7c4570dcdcd703f16
Wavesonics <adamwbrown@gmail.com>
2021-05-21 19:47:34 +0000

    lots fo work

--- controllers/base/LogsController.php
+++ controllers/base/LogsController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require_once('utils/BaseController.php');
@@ -63,70 +62,4 @@ class LogsController extends BaseController
 	}
 }
 
-=======
-<?php
-
-require_once('utils/BaseController.php');
-require_once('utils/b2_util.php');
-require_once('utils/MarkDown.php');
-
-class LogsController extends BaseController
-{
-	public function __construct( $config )
-	{
-		parent::__construct( false, $config );
-	}
-
-	public function urlStub()
-	{
-		return 'logs';
-	}
-
-	public function getTitle()
-	{
-		return 'Logs';
-	}
-
-	public function getRichTitle()
-	{
-		return 'Adventure.Rocks - Logs';
-	}
-
-	public function getRichDescription()
-	{
-		return 'Dispatches from our adventures';
-	}
-
-	public function getBody( $request, $todaysPhoto, $xtpl )
-	{
-		$db = getDb();
-
-		foreach( $db->blogs()->where('is_published', 1)->order('date_display DESC') as $entry )
-		{
-			$content = $entry['content'];
-			$contentSummary = substr( $content, 0, strpos( $content, "\n" ) );
-
-			$bodyText = getMarkdown()->parse($contentSummary);
-
-			$blogDate = new DateTime( utcToPst($entry['date_display']) );
-			$blogDateStr = $blogDate->format('M d');
-
-			$heroPhotoId = $entry['hero_photo_id'];
-			$photo = getPhoto( $heroPhotoId, true, 1024, 1024 );
-
-			$xtpl->assign_file( 'BODY_FILE', 'templates/logs.html' );
-
-			$xtpl->assign( 'ENTRY_ID', $entry['id'] );
-			$xtpl->assign( 'BLOG_DATE', $blogDateStr );
-			$xtpl->assign( 'BLOG_TITLE', $entry['title'] );
-			$xtpl->assign( 'BLOG_CONTENT', $bodyText );
-			$xtpl->assign( 'BLOG_HERO_PHOTO_URL', $photo->image );
-			$xtpl->parse( 'main.body.log_entry' );
-		}
-
-		$xtpl->parse( 'main.body' );
-	}
-}
-
->>>>>>> 0bc6615a0373328a595c25b5a586a0a01329692a
 ?>
--- controllers/base/TimeLineController.php
+++ controllers/base/TimeLineController.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <?php
 
 require_once('utils/BaseController.php');
@@ -70,77 +69,4 @@ class TimeLineController extends BaseController
 	}
 }
 
-=======
-<?php
-
-require_once('utils/BaseController.php');
-require_once('utils/b2_util.php');
-
-class TimeLineController extends BaseController
-{
-	public function __construct( $config )
-	{
-		parent::__construct( false, $config );
-	}
-
-	public function urlStub()
-	{
-		return 'timeline';
-	}
-
-	public function getTitle()
-	{
-		return 'Timeline';
-	}
-
-	public function getRichTitle()
-	{
-		return 'Adventure.Rocks - Timeline';
-	}
-
-	public function getBody( $request, $todaysPhoto, $xtpl )
-	{
-		$this->addCssFile( '/external/vertical-timeline/style.css', $xtpl );
-		$this->addJsFile( '/external/vertical-timeline/main.js', $xtpl );
-
-		$this->addCssFile( '/css/timeline.css', $xtpl );
-
-		$xtpl->assign_file( 'BODY_FILE', 'templates/timeline.html' );
-
-		$jsonData = file_get_contents('timeline.json');
-		$data = json_decode($jsonData);
-
-		foreach( $data->timeline as $entry )
-		{
-			echo 'print line<br />';
-			$date = date( 'j M', $entry->date );
-			$xtpl->assign( 'ENTRY_DATE', $date );
-			$xtpl->assign( 'ENTRY_TITLE', $entry->title );
-			$xtpl->assign( 'ENTRY_DESCRIPTION', $entry->description );
-
-			$numPhotos = count($entry->photos);
-			if($numPhotos == 1)
-			{
-				$xtpl->parse( 'main.body.timeline_entry.photo_container_full' );
-			}
-			else
-			{
-				$xtpl->parse( 'main.body.timeline_entry.photo_container_gallery' );
-			}
-
-			foreach($entry->photos as $photo)
-			{
-				$photoUrl = b2GetPublicTimelinePhoto($photo);
-				$xtpl->assign( 'ENTRY_PHOTO', $photoUrl );
-				$xtpl->parse( 'main.body.timeline_entry.photo' );
-			}
-
-			$xtpl->parse( 'main.body.timeline_entry' );
-		}
-
-		$xtpl->parse( 'main.body' );
-	}
-}
-
->>>>>>> 0bc6615a0373328a595c25b5a586a0a01329692a
 ?>
